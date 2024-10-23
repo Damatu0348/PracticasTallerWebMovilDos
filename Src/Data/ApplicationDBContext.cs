e54a3950274e9d3c07a32e8266080adb56eb18ec
damatu2020 <106501841+damatu2020@users.noreply.github.com>
2024-10-23 16:18:09 -0300

    feat: Contexto creado

---
+++ Src/Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Src.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Src.Data
+{
+    public class ApplicationDBContext
+    {
+        public DbSet<Cliente> Clientes { get; set; } = null!;
+        public DbSet<Producto> Productos { get; set; } = null!;
+        public DbSet<Role> Roles {get; set;} = null!;
+    }
+}
--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ddf50f6f1d44a0cd39d4a64697736b90c0f1f3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+319daf790c3411810e617aafbdfc549d79057be0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
